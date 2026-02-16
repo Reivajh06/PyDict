a396a9dfed02d27b87439f37b973b918b4da0167
ReivajH06 Herrera <reivajh06@gmail.com>
2026-02-16 20:00:44 +0000

    Implementation of put function in progress

--- src/model/Dictionary.java
+++ src/model/Dictionary.java
@@ -3,15 +3,33 @@ package model;
 public class Dictionary<K, V> {
 
 	private static final int BASELENGTH = 8;
-	private static Object DUMMY = new Object();
 
-	private Object[] hashtable;
+	private int slotsFilled;
+	private final Entry<K, V> dummy = new Entry(0, null, null);
+
+	private Entry<K, V>[] hashtable;
 
 	public Dictionary() {
-		hashtable = new Object[BASELENGTH];
+		hashtable = new Entry[BASELENGTH];
+		slotsFilled = 0;
 	}
 
-	public void add(K key, V value) {
+	public void put(K key, V value) {
 
+		int index = key.hashCode() % hashtable.length;
+		Entry<K, V> positionValue = hashtable[index];
+
+		while(positionValue != null) {
+			if(positionValue != dummy && positionValue.key.equals(key)) {
+				hashtable[index] = new Entry<>(key.hashCode(), key, value);
+				break;
+			}
+		}
+
+		hashtable[index] = new Entry<>(key.hashCode(), key, value);
+		slotsFilled++;
+	}
+
+	record Entry<K, V> (int hashcode, K key, V value) {
 	}
 }
