292746982666d0fd7fada5d4927872cb559a5a5b
ReivajH06 <reivajh06@gmail.com>
2026-02-17 12:24:39 +0000

    Grow function, keys and values functions implemented

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
+import model.Dictionary;
+
+import java.util.Arrays;
+
 public class Main {
 
 	public static void main(String[] args) {
+		Dictionary<String, Integer> dict = new Dictionary<>();
+		dict.put("Aa", 2);
+		dict.put("Aa", 4);
+		dict.put("BB", 3);
+		dict.put("Blue", 5);
+		dict.put("El Serio", -8);
+		dict.put("El Gato", 7);
+		dict.put("El Pardo", 60);
+		dict.put("El dqadwada", 80);
+		dict.put("adawdwadad", 1);
+		dict.put("nhfhfhfhtho", 0);
 
+		System.out.println(Arrays.toString(dict.keys()));
 	}
 }
--- src/model/Dictionary.java
+++ src/model/Dictionary.java
@@ -1,35 +1,93 @@
 package model;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 public class Dictionary<K, V> {
 
+	private static final int EXPANSIONFACTOR = 75;
+
+	private static final int DKIXEMPTY = -1;
+	private static final int DKIXDUMMY = -2;
+	private static final int PERTURBSHIFT = 5;
 	private static final int BASELENGTH = 8;
 
-	private int slotsFilled;
-	private final Entry<K, V> dummy = new Entry(0, null, null);
+	private int[] indices;
+	private int mask;
 
-	private Entry<K, V>[] hashtable;
+	private Entry<K, V>[] entries;
+	private int nextEntryIndex = 0;
+
+	private int nItems;
 
 	public Dictionary() {
-		hashtable = new Entry[BASELENGTH];
-		slotsFilled = 0;
+		indices = new int[BASELENGTH];
+		Arrays.fill(indices, -1);
+		entries = new Entry[BASELENGTH];
+		mask = BASELENGTH - 1;
+
+		nItems = 0;
+	}
+
+	private void grow() {
+		Entry<K, V>[] entr = Arrays.copyOf(entries, entries.length);
+
+		entries = new Entry[entries.length * 2];
+		indices = new int[indices.length * 2];
+		Arrays.fill(indices, -1);
+		mask = indices.length - 1;
+		nItems = 0;
+		nextEntryIndex = 0;
+
+		for(Entry<K, V> entry : entr) {
+			if(entry == null) continue;
+			put(entry.key, entry.value);
+		}
 	}
 
 	public void put(K key, V value) {
-
-		int index = key.hashCode() % hashtable.length;
-		Entry<K, V> positionValue = hashtable[index];
-
-		while(positionValue != null) {
-			if(positionValue != dummy && positionValue.key.equals(key)) {
-				hashtable[index] = new Entry<>(key.hashCode(), key, value);
-				break;
-			}
+		if((nItems * 100) / indices.length >= EXPANSIONFACTOR) {
+			grow();
 		}
 
-		hashtable[index] = new Entry<>(key.hashCode(), key, value);
-		slotsFilled++;
+		//Remember to include a reference to the first dummy found,
+		//Finding a dummy may not lead to insertion as said key might already exists
+
+		int perturb = key.hashCode();
+		int j = perturb;
+		int index = j & mask;
+
+		while(indices[index] != DKIXDUMMY && indices[index] != DKIXEMPTY) {
+			if(entries[indices[index]].key.equals(key)) {
+				entries[indices[index]] = new Entry<>(key, value, key.hashCode());
+				return;
+			}
+
+			//unsigned shift
+			perturb >>>= PERTURBSHIFT;
+			j = (5 * j) + 1 + perturb;
+			index = j & mask;
+		}
+
+		entries[nextEntryIndex] = new Entry<>(key, value, key.hashCode());
+		indices[index] = nextEntryIndex++;
+		nItems++;
 	}
 
-	record Entry<K, V> (int hashcode, K key, V value) {
+	public K[] keys() {
+		return (K[]) Arrays.stream(entries)
+				.filter(Objects::nonNull)
+				.map(Entry::key)
+				.toArray(Object[]::new);
+	}
+
+	public V[] values() {
+		return (V[]) Arrays.stream(entries)
+				.filter(Objects::nonNull)
+				.map(Entry::value)
+				.toArray(Object[]::new);
+	}
+
+	record Entry<K, V>(K key, V value, int hash) {
 	}
 }
